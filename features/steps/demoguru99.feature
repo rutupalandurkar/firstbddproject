af524d82b0d736f0fe6d19ccfb92fd99e60bd496
rutupalandurkar <palandurkarrutu@gamil.com>
2023-04-26 10:27:57 +0530

    alpha

---
+++ features/steps/demoguru99.feature
@@ -0,0 +1,10 @@
+Feature: guru99 login
+  Scenario: to verify login page
+    Given : open CE browser
+    When : Enter url
+    Then : check guru99 login page
+   Scenario: to verify login page
+    Given : open CE browser
+    When : Enter url
+    Then : check fivefinger login page
+
---
+++ features/steps/guru99.py
@@ -0,0 +1,26 @@
+from behave import *
+# @given(u': open CE browser')
+# def step_impl(context):
+#     raise NotImplementedError(u'STEP: Given : open CE browser')
+#
+#
+# @when(u':Enter url')
+# def step_impl(context):
+#     raise NotImplementedError(u'STEP: When :Enter url')
+
+#
+# @then(u': check guru99 login page')
+# def step_impl(context):
+#     raise NotImplementedError(u'STEP: Then : check guru99 login page')
+
+
+
+@when(u': Enter url')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When : Enter url')
+
+
+@then(u': check fivefinger login page')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then : check fivefinger login page')
+
